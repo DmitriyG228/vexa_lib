3b2d932dcc80d2514ff6397529ce8ec867d30e69
DmitryG228 <2280905@gmail.com>
2024-05-05 21:00:46 +0300

    d

--- vexa/redis.py
+++ vexa/redis.py
@@ -28,3 +28,27 @@ async def get_redis(host = 'host.docker.internal',port=6380,db=0):
     await db.ping()
 
     return db
+
+
+
+async def get_connections_df(name, redis_client, min_length=1, pattern='*'):
+    """
+    Finds queues that match a given pattern and have a minimum number of items,
+    and returns a DataFrame with queue names and their lengths.
+    """
+    matching_queues = []
+    queue_lengths = []
+    cursor = '0'
+    while cursor != 0:
+        cursor, keys = await redis_client.scan(cursor, match=f'{name}:{pattern}', count=min_length)
+        for key in keys:
+            if await redis_client.type(key) == 'list' and await redis_client.llen(key) >= min_length:
+                matching_queues.append(key)
+                length = await redis_client.llen(key)
+                queue_lengths.append(length)
+
+    data = {
+        'queue': matching_queues,
+        'length': queue_lengths
+    }
+    return pd.DataFrame(data)
---
+++ vexa/tools.py
@@ -0,0 +1,27 @@
+def log(*args,file_path=None):
+    """
+    Logs the provided arguments into a CSV file along with a current timestamp.
+
+    Parameters:
+    - file_path: str, the path to the CSV file where data should be logged. Defaults to 'log.csv' in the current directory.
+    - *args: variable length argument list, the data to log in the CSV.
+    """
+
+    # Get the current timestamp
+    now = datetime.now()
+    timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
+
+    if file_path is None:
+        script_name = os.path.basename(sys.argv[0])
+        if script_name.endswith('.py'):
+            script_name = script_name[:-3]  # Remove .py extension
+        file_path = f"/logs/{script_name}_log.csv"
+
+    # Open the CSV file and append the new row
+    with open(file_path, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        # Add the timestamp as the first item in the row, followed by the provided arguments
+        writer.writerow([timestamp] + list(args))
+
+    print([timestamp] + list(args))
+    
